293cd58db8e21e5f122739196f8847d7d161f574
Irinaaissa <irinaalisa7506@gmail.com>
2023-12-19 21:07:15 +0200

    2

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,15 +1,10 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+
+
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -39,5 +34,15 @@ function convertMs(ms) {
     message: 'What would you like to add?'
 });
 
+const flatPickrOptions = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const date = new Date(selectedDates[0]);
+  },
+};
+const instflatpickr = flatpickr('input#datetime-picker', flatPickrOptions);
 
 
