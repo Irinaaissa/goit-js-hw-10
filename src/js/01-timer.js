53c519649bbd1c5b02d542a3e6c5ccca7ce29398
Irinaaissa <irinaalisa7506@gmail.com>
2023-12-22 14:01:22 +0200

    12

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,37 +1,63 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
-
-
-
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24; // Remaining days
 
-  iziToast.show({
-    title: 'Hey',
-    message: 'What would you like to add?'
+  const days = Math.floor(ms / day); // Remaining hours
+  const hours = Math.floor((ms % day) / hour); // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute); // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const refs = {
+  days: document.querySelector('.value[data-days]'),
+  timer: document.querySelector('.timer'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
+  startBtn: document.querySelector('button[data-start]'),
+  picker: document.getElementById('datetime-picker'),
+};
+
+refs.startBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartButtonClick);
+
+
+function insertDataToControls(dateObj) {
+  if (dateObj) {
+    const { days, hours, minutes, seconds } = dateObj;
+    if (String(days).length > 3) {
+      refs.timer.classList.add('timer-shift');
+    }
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = minutes;
+    refs.seconds.textContent = seconds;
+  } else {
+    refs.timer.classList.remove('timer-shift');
+    refs.days.textContent = '00';
+    refs.hours.textContent = '00';
+    refs.minutes.textContent = '00';
+    refs.seconds.textContent = '00';
+  }
+}
+
+iziToast.show({
+  message: 'Please choose a date in the future',
 });
 
 const flatPickrOptions = {
@@ -41,8 +67,75 @@ const flatPickrOptions = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     const date = new Date(selectedDates[0]);
+    if (date - new Date() > 0) {
+      userSelectedDate = date;
+      refs.startBtn.disabled = false;
+    } else {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        messageColor: '#fafafb',
+        backgroundColor: '#fc5a5a',
+      });
+    }
   },
 };
 const instflatpickr = flatpickr('input#datetime-picker', flatPickrOptions);
 
+let userSelectedDate = null;
+const interval = 1000;
+let intervalId;
 
+function onStartButtonClick() {
+  updateTimer();
+  intervalId = setInterval(updateTimer, interval);
+  changeControlsStatus();
+}
+
+function updateTimer() {
+  if (!userSelectedDate) return;
+
+  const restTime = userSelectedDate - new Date();
+  if (restTime <= 0) {
+    resetTimer();
+    return;
+  }
+  const formattedValues = convertMs(restTime);
+  insertDataToControls(formattedValues);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function resetTimer() {
+  userSelectedDate = new Date();
+  intervalId && clearInterval(intervalId);
+  intervalId = null;
+  instflatpickr.setDate(new Date());
+  insertDataToControls();
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = true;
+  refs.picker.disabled = false;
+}
+
+function changeControlsStatus() {
+  refs.startBtn.disabled = !refs.startBtn.disabled;
+  refs.picker.disabled = !refs.picker.disabled;
+  refs.stopBtn.disabled = !refs.stopBtn.disabled;
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,61 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+
+
+const form = document.querySelector('.form');
+const stateRadios = document.querySelectorAll('input[name="state"]');
+if (stateRadios.length > 0) stateRadios[0].checked = true;
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const stateValue = form.querySelector('input[name="state"]:checked').value;
+  const delay = form.delay.value;
+
+  const promise = new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      if (stateValue === 'fulfilled') {
+        fulfill(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise.then(fulfill).catch(reject);
+}
+
+
+
+function fulfill(delay) {
+  iziToast.show({
+    message: `Fulfilled promise in ${delay}ms`,
+    messageColor: '#fafafb',
+    messageSize: '16px',
+    backgroundColor: '#82C43C',
+    position: 'topRight',
+    closeOnEscape: true,
+    close: true,
+    icon: 'Icomoon',
+    iconUrl: `${icon2Url}`,
+    iconColor: '#fafafb',
+  }).show(); 
+}
+
+function reject(delay) {
+  iziToast.show({
+    message: `Rejected promise in ${delay}ms`,
+    messageColor: '#fafafb',
+    messageSize: '16px',
+    backgroundColor: '#fc5a5a',
+    position: 'topRight',
+    closeOnEscape: false,
+    close: true,
+    icon: 'Icomoon',
+    iconUrl: `${icon1Url}`,
+    iconColor: '#fafafb',
+  }).show(); 
+}
